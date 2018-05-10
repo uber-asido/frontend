34cc01dc5c87413236f40c97a2c3565125fc2d59
Arvydas Sidorenko <asido4@gmail.com>
2018-05-10 09:20:22 +0200

    Changed movie autocomplete item icon.

--- src/app/modules/map/search-autocomplete-icon.pipe.ts
+++ src/app/modules/map/search-autocomplete-icon.pipe.ts
@@ -6,7 +6,7 @@ import { AutocompleteItemType } from "./search-input.component";
 export class SearchAutocompleteIconPipe implements PipeTransform{
     transform(type: AutocompleteItemType): string {
         switch (type) {
-            case AutocompleteItemType.Movie: return "movie";
+            case AutocompleteItemType.Movie: return "local_movies";
             case AutocompleteItemType.Organization: return "business";
             case AutocompleteItemType.Person: return "person";
             default: throw Error(`Unknown type: ${type}`);
